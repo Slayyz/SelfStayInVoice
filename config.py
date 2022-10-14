318954185efe7624ed90677e05bb97a31315c198
Slayyz <82596593+Slayyz@users.noreply.github.com>
2022-10-14 13:38:07 +0200

    config

---
+++ config.py
@@ -0,0 +1,70 @@
+import yaml
+
+with open("config.yaml", 'r') as f:
+    cfg = yaml.safe_load(f)
+
+with open("token.txt", "r") as f:
+    with open("config.yaml", "w") as yml:
+        for line in f:
+            if "#" in line:
+                continue
+            data = line.split()
+            if type(cfg) is not dict:
+                cfg = {}
+            if 'tokens' in cfg:
+                cfg['tokens'].append({ 'token': data[0] })
+                print("ajout")
+            else:
+                cfg['tokens'] = []
+                cfg['tokens'].append({ 'token': data[0] })
+                print("ajout")
+        # print(cfg)
+        yaml.dump(cfg, yml)
+
+f = open('token.txt', 'r+')
+f.truncate(0)
+
+class Token_config:
+    token = None
+    guild_id = None
+    channel_id = None
+    self_mute = None
+    self_deaf = None
+
+class Default_token_config(Token_config):
+    __instance = None
+    guild_id = cfg['default']['guild_id']
+    channel_id = cfg['default']['channel_id']
+    self_mute = cfg['default']['self_mute']
+    self_deaf = cfg['default']['self_deaf']
+
+def get_default():
+    config = Token_config()
+    config.guild_id = cfg['default']['guild_id']
+    config.channel_id = cfg['default']['channel_id']
+    config.self_mute = cfg['default']['self_mute']
+    config.self_deaf = cfg['default']['self_deaf']
+
+    return config
+
+def get_tokens():
+    tokens = []
+    for token in cfg['tokens']:
+        config = Default_token_config()
+        config.token = token['token']
+        if 'guild_id' in token:
+            config.guild_id = token['guild_id']
+        if 'channel_id' in token:
+            config.channel_id = token['channel_id']
+        if 'self_mute' in token:
+            config.self_mute = token['self_mute']
+        if 'self_deaf' in token:
+            config.self_deaf = token['self_deaf']
+        tokens.append(config)
+    for i in tokens:
+        print(i.token)
+        print(i.guild_id)
+        print(i.channel_id)
+        print(i.self_mute)
+
+    return tokens
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from http import client
+from email.policy import default
 import threading
 import asyncio
 import discord
 import os
 from discord.ext import commands
 
-GUILD_ID = Guild Id
-CHANNEL_ID = Channel ID
+import config
+
+defaults = config.get_default()
+(GUILD_ID, CHANNEL_ID) = (defaults.guild_id, defaults.channel_id)
 
 def toz(token):
     asyncio.set_event_loop(asyncio.new_event_loop())
@@ -16,21 +18,14 @@ def toz(token):
         #print(f'Logged in as {client.user} ({client.user.id})')
         vc = discord.utils.get(client.get_guild(GUILD_ID).channels, id=CHANNEL_ID)
         await vc.guild.change_voice_state(channel=vc,
-                                        self_mute=True, #True or False
-                                        self_deaf=True) #True or False
+                                        self_mute=defaults.self_mute,
+                                        self_deaf=defaults.self_deaf)
     
         print(f"Logged in as {client.user} ({client.user.id}). Successfully joined {vc.name} ({vc.id}")
     client.run(token)
 
-with open("token.txt", "r") as f:
-    accounts = list()
-    for line in f:
-        if "#" in line:
-            continue
-        data = line.split()
-        accounts.append(data[0])
-
-tokens = (accounts)
+tok = config.get_tokens()
+tokens = (tok.token)
 
 clients = []
 
@@ -38,7 +33,6 @@ for i, token in enumerate(tokens):
     # print(f'I: {i}, K: {token}')
     clients.append(threading.Thread(target=toz, args=(token,)))
     clients[i].start()
-
 
 while True:
     if input() == 'exit':
