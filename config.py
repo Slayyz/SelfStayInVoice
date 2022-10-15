14e2a8c7b95cf7eae5f637e3f09a829880ebb803
Slayyz <82596593+Slayyz@users.noreply.github.com>
2022-10-15 16:45:20 +0200

    Final

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ with open("token.txt", "r") as f:
                 cfg['tokens'].append({ 'token': data[0] })
                 print("ajout")
         # print(cfg)
-        yaml.dump(cfg, yml)
+        yaml.dump(cfg, yml, sort_keys=False)
 
 f = open('token.txt', 'r+')
 f.truncate(0)
@@ -31,26 +31,18 @@ class Token_config:
     self_mute = None
     self_deaf = None
 
-class Default_token_config(Token_config):
-    __instance = None
-    guild_id = cfg['default']['guild_id']
-    channel_id = cfg['default']['channel_id']
-    self_mute = cfg['default']['self_mute']
-    self_deaf = cfg['default']['self_deaf']
-
 def get_default():
     config = Token_config()
     config.guild_id = cfg['default']['guild_id']
     config.channel_id = cfg['default']['channel_id']
     config.self_mute = cfg['default']['self_mute']
     config.self_deaf = cfg['default']['self_deaf']
-
     return config
 
 def get_tokens():
     tokens = []
     for token in cfg['tokens']:
-        config = Default_token_config()
+        config = get_default()
         config.token = token['token']
         if 'guild_id' in token:
             config.guild_id = token['guild_id']
@@ -61,10 +53,4 @@ def get_tokens():
         if 'self_deaf' in token:
             config.self_deaf = token['self_deaf']
         tokens.append(config)
-    for i in tokens:
-        print(i.token)
-        print(i.guild_id)
-        print(i.channel_id)
-        print(i.self_mute)
-
     return tokens
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from email.policy import default
 import threading
 import asyncio
 import discord
@@ -7,25 +6,23 @@ from discord.ext import commands
 
 import config
 
-defaults = config.get_default()
-(GUILD_ID, CHANNEL_ID) = (defaults.guild_id, defaults.channel_id)
-
 def toz(token):
     asyncio.set_event_loop(asyncio.new_event_loop())
     client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
     @client.event
     async def on_ready():
         #print(f'Logged in as {client.user} ({client.user.id})')
-        vc = discord.utils.get(client.get_guild(GUILD_ID).channels, id=CHANNEL_ID)
-        await vc.guild.change_voice_state(channel=vc,
-                                        self_mute=defaults.self_mute,
-                                        self_deaf=defaults.self_deaf)
-    
-        print(f"Logged in as {client.user} ({client.user.id}). Successfully joined {vc.name} ({vc.id}")
-    client.run(token)
 
-tok = config.get_tokens()
-tokens = (tok.token)
+        vc = discord.utils.get(client.get_guild(token.guild_id).channels, id=token.channel_id)
+        await vc.guild.change_voice_state(channel=vc,
+                                        self_mute=token.self_mute,
+                                        self_deaf=token.self_deaf)
+    
+        print(f"Logged in as {client.user} ({client.user.id}). Successfully joined {vc.name} ({vc.id})")
+    client.run(token.token)
+
+
+tokens = config.get_tokens()
 
 clients = []
 
